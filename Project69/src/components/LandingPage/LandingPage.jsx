fe3d036c1e881f4ebb79c84532ed9bc44f009ce6
amlanxbghn <amlanxconnect@gmail.com>
2024-08-05 01:19:52 +0530

    commit

---
+++ Project69/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LandingPage() {
+    return (
+        <div className="flex items-center justify-center h-screen w-screen">
+            <div className="text-white text-5xl font-inter">
+                Cross Platform Content Diversification Tool
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
