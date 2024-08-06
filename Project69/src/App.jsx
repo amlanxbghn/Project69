f8cfb0f9ea39d36a79365d961ab87567c39669e7
amlanxbghn <amlanxconnect@gmail.com>
2024-08-06 23:18:06 +0530

    about page

--- Project69/src/App.jsx
+++ Project69/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import LandingPage from './components/LandingPage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <LandingPage/>
+      <About/>
       <Footer/>
     </div>
   );
